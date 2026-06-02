4136b2bb08822b88fd28f7d8fad206133971d01b
Ayush Soni <soniayush9125@gmail.com>
2026-06-02 22:48:32 +0530

    Implement getCurrentChatters route and controller to fetch recent conversation partners

--- backend/Controllers/userHandlerController.js
+++ backend/Controllers/userHandlerController.js
@@ -1,3 +1,4 @@
+import Conversation from "../Models/conversation.js";
 import User from "../Models/userModels.js";
 
 export const getUserbySearch = async(req,res) => {
@@ -25,4 +26,31 @@ export const getUserbySearch = async(req,res) => {
         console.log(error);
         res.status(500).json({message: "Internal server error"});
     }
+} 
+
+export const getCurrentChatters = async(req,res) => {
+    try {
+        const currentUserId = req.user._id;
+        const currentChatters = await Conversation.find({
+            participants: currentUserId
+        }).sort({updatedAt: -1});
+        if(!currentChatters || currentChatters.length === 0){
+            return res.status(200).json([]);
+        }
+        const participantsIds = currentChatters.reduce((ids,conversation) => {
+                const otherParticipants = conversation.participants.filter(id => id!== currentUserId)
+                return [...ids, ...otherParticipants]
+        },[])
+        const otherParticipantsIds = participantsIds.filter(id => id.toString() !== currentUserId.toString());
+        const user = await User.find({
+            _id:{$in:otherParticipantsIds}
+        }).select("-password").select("-email");
+
+        const users = otherParticipantsIds.map(id => user.find(u => u._id.toString() === id.toString()));
+        return res.status(200).json(users);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({message: "Internal server error"});
+    }
 }
+    
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { getUserbySearch } from "../Controllers/userHandlerController.js";
+import { getCurrentChatters, getUserbySearch } from "../Controllers/userHandlerController.js";
 import isLogin from "../middleware/isLogin.js";
 
 const router = express.Router();
 
 router.get('/search',isLogin,getUserbySearch);
+router.get('/currentchatters',isLogin,getCurrentChatters);
 
 export default router;
